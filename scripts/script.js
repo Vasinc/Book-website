dda186fc1782f129a5d7cd75b9a311e8d1863902
Vasinc <lein0.jade@gmail.com>
2022-02-01 16:36:35 +0200

    Second commit (Header done, Why Section for Main)

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const fadersRight = document.querySelectorAll('.fader-right');
+const fadersLeft = document.querySelectorAll('.fader-left');
+
+const sectionOptions = {
+    threshold: .5
+}
+
+const sectionObserverSide = new IntersectionObserver(function(entries, sectionObserver) {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("appear-side", entry.isIntersecting);
+    });
+}, sectionOptions);
+
+fadersRight.forEach(fader => {
+    sectionObserverSide.observe(fader);
+})
+
+fadersLeft.forEach(fader => {
+    sectionObserverSide.observe(fader);
+})
+
+// reasonCards.forEach(card => {
+//     sectionObserverSide.observe(card);
+// })
