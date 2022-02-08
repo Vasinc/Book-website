7a3ee38568e61ca8a63f38fefcc53512ccebf34f
Vasinc <lein0.jade@gmail.com>
2022-02-08 21:20:55 +0200

    Forth commit(Quotes Section added)

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
 const fadersRight = document.querySelectorAll('.fader-right');
 const fadersLeft = document.querySelectorAll('.fader-left');
+const quotesBtn = document.querySelector('.info-btn');
+const quotesInfoText = document.querySelector('.info-text');
+const quotes = document.querySelectorAll('.quote');
 
 const sectionOptions = {
     threshold: .5
@@ -18,3 +21,21 @@ fadersRight.forEach(fader => {
 fadersLeft.forEach(fader => {
     sectionObserverSide.observe(fader);
 })
+
+quotesBtn.addEventListener('click', () => {
+    quotesInfoText.classList.toggle('text-show')
+})
+
+quotesBtn.addEventListener('mousedown', () => {
+    quotesBtn.classList.add('btn-pushed');
+})
+
+quotesBtn.addEventListener('mouseup', () => {
+    quotesBtn.classList.remove('btn-pushed');
+})
+
+quotes.forEach(quote => {
+    quote.addEventListener('click', () => {
+        quote.classList.toggle('bej');
+    });
+});
