4a2fd5a9422c2e821fee2b481a92738dc66987e5
Vasinc <lein0.jade@gmail.com>
2022-02-10 17:41:47 +0200

    Fifth commit(Bookshelf + Footer)

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,17 @@
+const body = document.body;
+
 const fadersRight = document.querySelectorAll('.fader-right');
 const fadersLeft = document.querySelectorAll('.fader-left');
 const quotesBtn = document.querySelector('.info-btn');
 const quotesInfoText = document.querySelector('.info-text');
 const quotes = document.querySelectorAll('.quote');
+
+const book = document.getElementById('special-book');
+const backdrop = document.querySelector('.backdrop');
+const pagesSection = document.querySelector('.pages-section');
+const pagesSlider = document.querySelector('.pages-slider');
+const pagesLeftBtn = document.querySelector('.page-button__left');
+const pagesRightBtn = document.querySelector('.page-button__right');
 
 const sectionOptions = {
     threshold: .5
@@ -39,3 +48,38 @@ quotes.forEach(quote => {
         quote.classList.toggle('bej');
     });
 });
+
+let counter = 0;
+const size = 340;
+
+pagesRightBtn.addEventListener('click', () => {
+    if(counter >= 5) {
+        return;
+    }
+    pagesSlider.style.transition = 'transform .4s ease-out'
+    counter++;
+    pagesSlider.style.transform = `translateX(${-size * counter}px)`
+});
+
+pagesLeftBtn.addEventListener('click', () => {
+    if(counter <= 0) {
+        return;
+    }
+    pagesSlider.style.transition = 'transform .4s ease-out'
+    counter--;
+    pagesSlider.style.transform = `translateX(${-size * counter}px)`
+})
+
+function togglePages () {
+    backdrop.classList.toggle('display-block');
+    pagesSection.classList.toggle('display-flex');
+    body.classList.toggle('overflow');
+}
+
+book.addEventListener('click', () => {
+    togglePages();
+});
+
+backdrop.addEventListener('click', () => {
+    togglePages();
+})
